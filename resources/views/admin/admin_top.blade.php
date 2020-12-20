b47a80c907e3168ade016d98863f9dd75de0830b
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-12-20 23:52:18 +0900

    fix: bootstrapとfontawesomeをcdnで配信

--- resources/views/admin/admin_top.blade.php
+++ resources/views/admin/admin_top.blade.php
@@ -52,7 +52,12 @@
     <meta name="theme-color" content="#ffffff">
 
     <!-- Fontawesome -->
-    <link type="text/css" href="{{'vendor/@fortawesome/fontawesome-free/css/all.min.css'}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+
+    <!-- Bootstrap5 -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 
     <!-- Notyf -->
     <link type="text/css" href="{{'vendor/notyf/notyf.min.css'}}" rel="stylesheet">
@@ -854,7 +859,6 @@
 
     <!-- Core -->
     <script src="{{'vendor/popper.js/dist/umd/popper.min.js'}}"></script>
-    <script src="{{'vendor/bootstrap/dist/js/bootstrap.min.js'}}"></script>
 
     <!-- Vendor JS -->
     <script src="{{'vendor/onscreen/dist/on-screen.umd.min.js'}}"></script>

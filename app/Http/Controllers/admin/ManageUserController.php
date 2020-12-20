a3b1d21942703ffce10e2907fc5d15f51046f48a
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-12-20 21:50:15 +0900

    change: モデルは app/Models 以下に配置する

--- app/Http/Controllers/admin/ManageUserController.php
+++ app/Http/Controllers/admin/ManageUserController.php
@@ -2,7 +2,7 @@
 namespace App\Http\Controllers\admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
 class ManageUserController extends Controller
 {
 	function showUserList(){

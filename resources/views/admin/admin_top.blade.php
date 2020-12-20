5d1eb1a686a874c4d883be7c2a685c3e5185a123
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-12-20 23:55:47 +0900

    fix

--- resources/views/admin/admin_top.blade.php
+++ resources/views/admin/admin_top.blade.php
@@ -56,11 +56,9 @@
 
     <!-- Bootstrap5 -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
 
     <!-- Notyf -->
-    <link type="text/css" href="{{'vendor/notyf/notyf.min.css'}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
 
     <!-- Volt CSS -->
     <link type="text/css" href="{{'css/volt.css'}}" rel="stylesheet">
@@ -857,6 +855,10 @@
         </footer>
     </main>
 
+    <!-- Bootstrap5 -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+
     <!-- Core -->
     <script src="{{'vendor/popper.js/dist/umd/popper.min.js'}}"></script>
 
@@ -876,7 +878,7 @@
     <script src="{{'vendor/countup.js/dist/countUp.umd.js'}}"></script>
 
     <!-- Notyf -->
-    <script src="{{'vendor/notyf/notyf.min.js'}}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
 
     <!-- Charts -->
     <script src="{{'vendor/chartist/dist/chartist.min.js'}}"></script>

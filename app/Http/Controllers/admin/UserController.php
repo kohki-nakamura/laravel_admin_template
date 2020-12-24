c6b1be82e04320b9902aee4c78bb94e9ab339e85
kohki-nakamura <42920380+kohki-nakamura@users.noreply.github.com>
2020-12-24 14:17:44 +0900

    fix: インデント修正

--- app/Http/Controllers/admin/UserController.php
+++ app/Http/Controllers/admin/UserController.php
@@ -6,96 +6,97 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
-	/**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index(){
-		$users = User::orderBy("id", "desc")->paginate(10);
-		return view("admin.users.index", [
-			"users" => $users
-		]);
-	}
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index(){
+    $users = User::orderBy("id", "desc")->paginate(10);
+    return view("admin.users.index", [
+      "users" => $users
+    ]);
+  }
 
 	/**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.users.create');
-    }
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+      return view('admin.users.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $user = new User;
-		$this->validate($request, User::$store_rules);
-        User::create([
-            'name' => $request['name'],
-            'email' => $request['email'],
-            'password' => Hash::make($request['password']),
-        ]);
-		return redirect('admin/users/'.$user->id);
-    }
-
-	/**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id){
-		$user = User::find($id);
-		return view("admin.users.show", [
-			"user" => $user
-		]);
-	}
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $user = new User;
+    $this->validate($request, User::$store_rules);
+    User::create([
+      'name' => $request['name'],
+      'email' => $request['email'],
+      'password' => Hash::make($request['password']),
+    ]);
+    return redirect('admin/users/'.$user->id);
+  }
 
 	/**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    $user = User::find($id);
+    return view("admin.users.show", [
+      "user" => $user
+    ]);
+  }
+
+	/**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
 	public function edit($id)
-    {
+  {
 		$user = User::find($id);
-        return view('admin.users.edit', ['user' => $user,]);
-    }
+      return view('admin.users.edit', ['user' => $user,]);
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-		$user = User::find($id);
-		$this->validate($request, User::$update_rules);
-        $user->fill($request->except('_token', '_method'))->save();
-		return redirect('admin/users/'.$id);
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $user = User::find($id);
+    $this->validate($request, User::$update_rules);
+    $user->fill($request->except('_token', '_method'))->save();
+  return redirect('admin/users/'.$id);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-		$user = User::find($id);
-		$user->delete();
-		return redirect('admin/users');
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    $user = User::find($id);
+    $user->delete();
+    return redirect('admin/users');
+  }
 }
